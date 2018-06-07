5f572f1ff23d251a62db410b19fc4e0bc7434049
zhengwei.zhou <anbangzhiguo@163.com>
2018-06-07 17:04:15 +0800

    commit

--- src/main/java/com/srpingboottest/gittest/GittestApplication.java
+++ src/main/java/com/srpingboottest/gittest/GittestApplication.java
@@ -2,11 +2,21 @@ package com.srpingboottest.gittest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class GittestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GittestApplication.class, args);
+	}
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello";
 	}
 }
